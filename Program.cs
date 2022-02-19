bf4ecd40b20a9ff659495d8afa71ea514a347b6b
William McGonagle <wpmcgonagle@gmail.com>
2022-02-18 21:12:22 -0500

    🚀 Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace srl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+# Simple Rendering Layer
